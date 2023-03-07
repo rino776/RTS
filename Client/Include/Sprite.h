9c5232692fbdf9fe2dddefb461974913e429813a
rino776 <ryan.harrold@connect.qut.edu.au>
2023-03-08 00:46:22 +1000

    added sprite class

---
+++ Client/Include/Sprite.h
@@ -0,0 +1,24 @@
+#pragma once
+#include "Component.h"
+namespace core{
+class Sprite :
+    public Component
+{
+public:
+    void setWidth(float width) { m_width = width; }
+    void setHeight(float height) { m_height = height; }
+    void setTexId(int texID) { m_texID = texID; };
+
+    float width() { return m_width; }
+    float height() { return m_height; }
+    int texID() { return m_texID; }
+
+
+private:
+    float m_width;
+    float m_height;
+    int m_texID;
+};
+
+}
+
